874c74c8a10c4ef7a3203e27c28a4b5575aaab8d
jasonoh22 <jasonoh22@naver.com>
2023-11-16 00:16:08 +0900

    step37 생성자 주입

--- src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
@@ -9,8 +9,13 @@ import java.util.List;
 
 @Service //저는 단 한번만 생성되고, 그 이우에는 재사용되는 객체입니다.
 public class ArticleService {
-    @Autowired
-    private  ArticleRepository articleRepository;
+    /*@Autowired 필드 주입 --> 객체만들고 나중에 받는것
+    private final ArticleRepository articleRepository;  final 불가능 왜냐면 만들면 이미 null 이 들어가 바꿀 수 없기 때문이다. */
+    private  final ArticleRepository articleRepository;// 생성자로 주입할땐 final 가능 , 만들자마자 바로 예시로(칼) 이 들어가기 떄문에 된다.
+    @Autowired // 생성자 주입 --> 객체만들면서 바로 주기
+    public ArticleService(ArticleRepository articleRepository){
+        this.articleRepository =articleRepository;
+    }
 
     public Article write(String body, String title) {
         Article article = new Article(title,body);
